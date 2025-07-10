3b71da944b2b3cbf46ac62d9c6ba8505d9e1cf79
usanase1 <emelineu072@gmail.com>
2025-07-10 22:22:25 +0200

    modify

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -5,26 +5,15 @@ import Image from 'next/image';
 import WhoWeAreSection from '@/components/about/WhoWeAreSection';
 import CoreTeamSection from '@/components/about/CoreTeamSection';
 import BannerSection from '@/components/about/BannerSection';
-import ProductsSection from '@/components/about/ProductsSection';
-import NewsletterSection from '@/components/about/NewsletterSection';
 import aboutTeamData from '@/components/about/aboutTeamData';
 import aboutProductsData from '@/components/about/aboutProductsData';
+import DealCategoriesSection from '@/components/home/DealCategoriesSection';
 
 export default function About() {
   return (
-    <div className="bg-white">
-      <div
-        className="
-          w-[1920px]
-          h-[3316px]
-          bg-white
-          rounded-[4px]
-          opacity-100
-          mx-auto
-          overflow-hidden
-        "
-      >
-        <div className="max-w-7xl mx-auto px-4 py-16 space-y-24">
+    <div className="bg-white min-h-screen flex flex-col">
+      <div className="w-full max-w-7xl mx-auto rounded-[4px] opacity-100 overflow-hidden flex-1">
+        <div className="px-4 py-8 md:py-16 space-y-16 md:space-y-24">
           {/* Who we are */}
           <WhoWeAreSection />
           {/* Core team */}
@@ -32,11 +21,9 @@ export default function About() {
         </div>
         {/* Banner image made full screen width here */}
         <BannerSection />
-        <div className="max-w-7xl mx-auto px-4 py-16 space-y-24">
+        <div className="px-4 py-8 md:py-16 space-y-16 md:space-y-24">
           {/* Products section */}
-          <ProductsSection products={aboutProductsData} />
-          {/* Newsletter */}
-          <NewsletterSection />
+          <DealCategoriesSection />
         </div>
       </div>
     </div>
