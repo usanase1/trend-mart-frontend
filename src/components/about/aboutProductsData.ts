9b6625442212162e370106190d86d53e577c4097
usanase1 <emelineu072@gmail.com>
2025-07-10 22:22:25 +0200

    modify

--- src/components/about/aboutProductsData.ts
+++ src/components/about/aboutProductsData.ts
@@ -1,11 +1,22 @@
 const aboutProductsData = [
-  { title: 'Bose Sport Earbuds', price: '$1,500' },
-  { title: 'Samsung Galaxy S21', price: '$1,500' },
-  { title: 'Sony DSCQX High-Zoom', price: '$1,500' },
-  { title: 'TOZO T6 Wireless Earbuds', price: '$1,500' },
-  { title: '4K UHD LED TV', price: '$1,500' },
-  { title: 'Gaming Phone', price: '$1,500' },
-  { title: 'Portable Washing Machine', price: '$1,500' },
-  { title: '1080p Pan Tilt Camera', price: '$1,500' },
+  // FLASH SALE TODAY
+  { title: 'Bose Sport Earbuds -Wireless Earphones -Bluetooth In Ear...', price: '$1,500', image: '/images/image-1.png' },
+  { title: 'Simple Mobile 4G LTE Prepaid Smartphone', price: '$1,500', image: '/images/image-3.png' },
+  { title: '4K UHD LED Smart TV with Chromecast Built-in', price: '$1,500', image: '/images/image-4.png' },
+
+  // BEST SELLERS
+  { title: 'Samsung Electronics Samsung Galexy S21 5G', price: '$1,500', image: '/images/image-2.png' },
+  { title: 'Simple Mobile 5G LTE Galexy 12 Mini 512GB Gaming Phone', price: '$1,500', image: '/images/image-5.png' },
+  { title: 'Sony DSCHX8 High Zoom Point & Shoot Camera', price: '$1,500', image: '/images/image-6.png' },
+
+  // TOP RATED
+  { title: 'Portable Wshing Machine, 11lbs capacity Model 18NMF...', price: '$1,500', image: '/images/image-7.png' },
+  { title: 'Sony DSCHX8 High Zoom Point & Shoot Camera', price: '$1,500', image: '/images/image-8.png' },
+  { title: 'Dell Optiplex 7000x7480 All-in-One Computer Monitor', price: '$1,500', image: '/images/image-9.png' },
+
+  // NEW ARRIVAL
+  { title: 'TOZO T6 True Wireless Earbuds Bluetooth Headpho...', price: '$1,500', image: '/images/image-10.png' },
+  { title: 'JBL FLIP 4 -Waterproof Portable Bluetooth Speaker...', price: '$1,500', image: '/images/image-11.png' },
+  { title: 'Wyze Cam Pan v2 1080p Pan/Tilt/Zoom Wi-Fi Indoor Smar...', price: '$1,500', image: '/images/image-12.png' },
 ];
 export default aboutProductsData; 
